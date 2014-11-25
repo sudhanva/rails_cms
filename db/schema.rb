968de1304b3c2949577674f7ebdc8c050fc524be
sudhanva sa <sudhanva.sa@gmail.com>
2014-11-25 18:21:28 +0530

    Added alter table migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125060017) do
+ActiveRecord::Schema.define(version: 20141125070828) do
+
+  create_table "admin_users", force: true do |t|
+    t.string   "first_name",      limit: 10
+    t.string   "last_name",       limit: 5
+    t.string   "email",           limit: 100, default: "", null: false
+    t.string   "username",        limit: 25
+    t.string   "hashed_password", limit: 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
 
   create_table "pages", force: true do |t|
     t.integer  "subject_id"
@@ -43,15 +55,6 @@ ActiveRecord::Schema.define(version: 20141125060017) do
     t.string   "name"
     t.integer  "position"
     t.boolean  "visible",    default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "first_name", limit: 10
-    t.string   "last_name",  limit: 5
-    t.string   "email",                 default: "", null: false
-    t.string   "password",   limit: 40
     t.datetime "created_at"
     t.datetime "updated_at"
   end
